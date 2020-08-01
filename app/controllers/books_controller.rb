466cd1de16e2aa43931d531c07988917991c07fa
vagrant <vagrant@localhost.localdomain>
2020-08-01 07:33:51 +0000

    [create]bookers:BasicEdition

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,53 @@
+class BooksController < ApplicationController
+
+  def top
+  end
+
+  def index
+    @lists = Book.all
+    @create = Book.new
+  end
+
+  def create
+    @create = Book.new(list_params)
+    if @create.save
+      flash[:notice] = 'Book was successfully created.'
+      redirect_to book_path(@create.id)
+    else
+      @lists = Book.all
+      render :index
+    end
+  end
+
+  def show
+    @detail = Book.find(params[:id])
+  end
+
+  def edit
+    @detail = Book.find(params[:id])
+  end
+
+  def update
+    @detail = Book.find(params[:id])
+    if @detail.update(list_params)
+      flash[:notice] = 'Book was successfully updated.'
+      redirect_to book_path(@detail.id)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @kill = Book.find(params[:id])
+    @kill.destroy
+    redirect_to books_path
+    flash[:notice] = 'Book was successfully destroyed.'
+  end
+
+  private
+  # ストロングパラメータ
+  def list_params
+    params.require(:book).permit(:title, :body)
+  end
+
+end
